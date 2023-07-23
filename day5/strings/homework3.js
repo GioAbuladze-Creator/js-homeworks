eb648fccc11698183c50e86be1618c3aaf6d4bc0
GioAbuladze-Creator <giorgi.abuladze.1@btu.edu.ge>
2023-07-23 17:50:50 +0400

    fixed unnecessary counting

--- day5/strings/homework3.js
+++ day5/strings/homework3.js
@@ -8,6 +8,8 @@ function searchWord(target,find){
         for(let i=0;i<target.length;i++){
             if (target.substring(i,i+find.length)==find){
                 count++;
+                //to avoid unnecessary count, in (aaa,aa) count == 1 instead of 2
+                i+=find.length-1;
             }
         }
         console.log(`${find} was found ${count} times.`)
@@ -18,3 +20,4 @@ function searchWord(target,find){
 searchWord("The quick brown fox", "fox"); // "'fox' was found 1 times."
 searchWord("aa, bb, cc, dd, aa", "aa"); // "'aa' was found 2 times."
 searchWord("sunshine", "sun"); // "'sun' was found 1 times."
+searchWord("aaa", "aa"); // "'aa' was found 1 time."
