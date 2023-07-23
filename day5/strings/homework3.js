e7b7878efa55f2bf167f56a6b72ce89eeede5ba7
GioAbuladze-Creator <giorgi.abuladze.1@btu.edu.ge>
2023-07-23 17:53:59 +0400

    fixed case sensitivity issue

--- day5/strings/homework3.js
+++ day5/strings/homework3.js
@@ -6,7 +6,7 @@ function searchWord(target,find){
         }
         let count=0;
         for(let i=0;i<target.length;i++){
-            if (target.substring(i,i+find.length)==find){
+            if (target.substring(i,i+find.length).toLowerCase()==find.toLocaleLowerCase()){
                 count++;
                 //to avoid unnecessary count, in (aaa,aa) count == 1 instead of 2
                 i+=find.length-1;
@@ -18,6 +18,6 @@ function searchWord(target,find){
     }
 }
 searchWord("The quick brown fox", "fox"); // "'fox' was found 1 times."
-searchWord("aa, bb, cc, dd, aa", "aa"); // "'aa' was found 2 times."
+searchWord("Aa, bb, cc, dd, aa", "aa"); // "'aa' was found 2 times."
 searchWord("sunshine", "sun"); // "'sun' was found 1 times."
 searchWord("aaa", "aa"); // "'aa' was found 1 time."
